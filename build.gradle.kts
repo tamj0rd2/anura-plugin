914f368153e3557376574de160ecdde192778b3a
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-04-23 09:15:57 +0100

    Set explicit version for plugin verifier

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,7 @@ dependencies {
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 
-        instrumentationTools()
-        pluginVerifier()
+        pluginVerifier(version="1.383")
         zipSigner()
         testFramework(TestFrameworkType.Platform)
     }
